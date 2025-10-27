ba432e2d34a8f68d6b7ad7a2f3bd65e7171f99ad
Rafael Cordero <recordero04@ucatolica.edu.co>
2025-10-26 20:22:45 -0500

    add: crear la clase categoria, con su id

--- modelsTienda.py
+++ modelsTienda.py
@@ -4,3 +4,6 @@ from sqlmodel import SQLModel, Field, Relationship
 class categoriaBase(BaseModel):
     name: str | None = Field(description="Nombre de la categoria")
     description: str | None = Field(description="Descripción de la categoria")
+
+class categoria(categoriaBase, table=True):
+    id: int | None = Field(default = None, primary_key = True)
