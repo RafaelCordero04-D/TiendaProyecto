ed4edc3342c3232dba352409c73b90da186055db
Rafael Cordero <recordero04@ucatolica.edu.co>
2025-10-28 19:19:05 -0500

    FEAT: Inicializar aplicación FastAPI y configurar rutas de categoiria.py
    Configura la instancia principal de FastAPI.
    
    -Se utuliza el event lifespan para inicializar la base de datos y crear tablas (create_tables)
    
    -Se incluye el router de categoria.py bajo el prefijo /categoria

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+import categoria
+from TiendaDb import create_tables
+app = FastAPI(lefespan = create_tables, tittle="Sistema de gestión de Tienda Online")
+app.include_router(categoria.router, tags=["categoria"], prefix="/categoria")
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
