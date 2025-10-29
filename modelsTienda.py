3bd240d4efba0d705517876abdf37de21b2062a1
Rafael Cordero <recordero04@ucatolica.edu.co>
2025-10-29 06:42:37 -0500

    FIX: En el schema de categoriaBase y productoBase, intercambiar BaseModel por SQLModel

--- modelsTienda.py
+++ modelsTienda.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 from sqlmodel import SQLModel, Field, Relationship
 
-class categoriaBase(BaseModel):
+class categoriaBase(SQLModel):
     name: str | None = Field(description="Nombre de la categoria")
     description: str | None = Field(description="Descripción de la categoria")
     status: bool = Field(description="True= activate, False= deactivate", default = True)
@@ -17,17 +17,17 @@ class categoriaUpdate(categoriaBase):
     pass
 
 
-class productoBase(BaseModel):
+class productoBase(SQLModel):
     name:str | None = Field(description="Nombre del producto")
     price:float | None = Field(description="Precio del producto")
     stock:int | None = Field(description="Stock del producto")
-    description: str | None = Fiel(description="Descripcion del produto")
+    description: str | None = Field(description="Descripcion del produto")
     status: bool | None = Field(description= "True= activate, False= deactivate", default = True)
 
 class producto(productoBase, table=True):
     id: int | None = Field(default= None, primary_key= True)
     categoria_id: int = Field(foreign_key= "categoria.id")
-    categoria: Categoria = Relationship(back_populates="productos")
+    categoria: "categoria" = Relationship(back_populates="productos")
 
 class productoCreate(productoBase):
     categoria_id: int = Field(foreign_key="categoria.id")
