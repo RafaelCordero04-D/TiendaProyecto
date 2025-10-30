ca709ca1be3f911536b6754b782e90bf23ac624e
Rafael Cordero <recordero04@ucatolica.edu.co>
2025-10-29 22:24:53 -0500

    FEAT:Integra el router de productos en la aplicación principal
    Configura la aplicación FastAPI para incluir todas las rutas definidas en `router_producto.py`.
    
    - Asigna el prefijo de URL `/producto` a todas las rutas del router.
    - Agrega la etiqueta `producto` para una mejor organización

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from sqlmodel import SQLModel
 from TiendaDb import engine
 import router_categoria
+import router_producto
 from TiendaDb import create_tables
 app = FastAPI(lefespan = create_tables, tittle="Sistema de gestión de Tienda Online")
 
@@ -9,6 +10,7 @@ app = FastAPI(lefespan = create_tables, tittle="Sistema de gestión de Tienda On
 def on_startup():
     SQLModel.metadata.create_all(engine)
 app.include_router(router_categoria.router, tags=["categoria"], prefix="/categoria")
+app.include_router(router_producto.router, tags=["producto"], prefix="/producto")
 
 
 
